b8c624a06fad55e233c64cd2b4fb559fca248ec8
Jose Zamora <161581683+novah451@users.noreply.github.com>
2024-03-07 13:33:22 -0800

    directories and db schema

---
+++ README.md
@@ -0,0 +1,7 @@
+# CMPS 3420 - Group Project
+
+**Developed by**: 
+    - Snehal Kumar
+    - Joseph Rivera
+    - Braden Stitt
+    - Jose Zamora
---
+++ db/schema.sql
@@ -0,0 +1,156 @@
+-- Temporarily disable foreign keys while creating tables with circualar dependencies
+
+SET foreign_key_checks = 0;
+
+-- Drop all tables
+
+DROP TABLE IF EXISTS Venue;
+DROP TABLE IF EXISTS Customer;
+DROP TABLE IF EXISTS Artist;
+DROP TABLE IF EXISTS Occasion;
+DROP TABLE IF EXISTS Attends;
+DROP TABLE IF EXISTS Performed;
+DROP TABLE IF EXISTS Follows;
+DROP TABLE IF EXISTS Venue_PhoneNumber;
+DROP TABLE IF EXISTS Event_Image;
+DROP TABLE IF EXISTS Event_Price;
+DROP TABLE IF EXISTS Artist_Genre;
+DROP TABLE IF EXISTS Customer_PhoneNumber;
+
+-- Create new sets of tables
+
+-- Venue(VID, VAddress, VName, Capacity)
+CREATE TABLE Venue (
+    ID int unsigned NOT NULL PRIMARY KEY,
+    Venue_Name varchar(50),
+    Venue_Address varchar(256),
+    Capacity int unsigned
+);
+
+-- Customer(CID, Username, Address, Date of Birth, Created at, Password)
+CREATE TABLE Customer {
+    ID int unsigned NOT NULL PRIMARY KEY,
+    Customer_Username varchar(50),
+    Customer_Password varchar(256) NOT NULL,
+    Customer_Address varchar(256),
+    Customer_DOB date,
+    Created_At varchar(10)
+};
+
+-- Artist(AName)
+CREATE TABLE Artist (
+    Enum int NOT NULL AUTO_INCREMENT, 
+    AName varchar(50) NOT NULL PRIMARY KEY
+);
+
+-- Jose:    'Event' already exists in SQL, so I changed the name to 'Occasion'
+-- Event(VID, EID, EName, Description, Time, Type, Date)
+-- FK VID refers to Venue(ID) (NOT NULL)
+CREATE TABLE Occasion (
+    Venue_ID int unsigned NOT NULL,
+    ID int unsigned NOT NULL,
+    Event_Name varchar(50),
+    Event_Description varchar(256),
+    -- Jose:    If we want to add a constraint to make sure the user is 18+,
+    --          We can add that on the server side or by adding 'CHECK' here
+    Event_Date_and_Time timestamp,
+    Event_Type varchar(50),
+    PRIMARY KEY (Venue_ID, ID),
+    FOREIGN KEY (Venue_ID) REFERENCES Venue(ID) 
+);
+
+-- Attends(VID, EID, CID)
+-- FK VID refers to Venue(ID) (NOT NULL)
+-- FK EID refers to Occasion(ID) (NOT NULL)
+-- FK CID refers to Customer(ID) (NOT NULL)
+CREATE TABLE Attends (
+    Venue_ID int unsigned NOT NULL,
+    Event_ID int unsigned NOT NULL,
+    Customer_ID int unsigned NOT NULL,
+    PRIMARY KEY (Venue_ID, Event_ID, Customer_ID)
+    FOREIGN KEY (Venue_ID) REFERENCES Venue(ID),
+    FOREIGN KEY (Event_ID) REFERENCES Occasion(ID),
+    FOREIGN KEY (Customer_ID) REFERENCES Customer(ID)
+);
+
+-- Performed(VID, EID, AName)
+-- FK VID refers to Venue(ID) (NOT NULL)
+-- FK EID refers to Occasion(ID) (NOT NULL)
+-- FK AName refers to Artist(AName) (NOT NULL)
+CREATE TABLE Performed (
+    Venue_ID int unsigned NOT NULL,
+    Event_ID int unsigned NOT NULL,
+    Artist_Name varchar(50) NOT NULL,
+    PRIMARY KEY (Venue_ID, Event_ID, Artist_Name),
+    FOREIGN KEY (Venue_ID) REFERENCES Venue(ID),
+    FOREIGN KEY (Event_ID) REFERENCES Occasion(ID),
+    FOREIGN KEY (Artist_Name) REFERENCES Artist(AName)
+);
+
+-- Follows(AName, CID)
+-- FK AName refers to Artist(AName)
+-- FK CID refers to Customer(ID)
+CREATE TABLE Follows (
+    Artist_Name varchar(50) NOT NULL,
+    Customer_ID int unsigned NOT NULL,
+    PRIMARY KEY (Artist_Name, Customer_ID),
+    FOREIGN KEY (Artist_Name) REFERENCES Artist(AName),
+    FOREIGN KEY (Customer_ID) REFERENCES Customer(ID)
+);
+
+-- Venue_PhoneNumber(VID, Phone_Number)
+-- FK VID refers to Venue(ID) (NOT NULL)
+CREATE TABLE Venue_PhoneNumber (
+    Venue_ID int unsigned NOT NULL,
+    Phone_Number varchar(25),
+    PRIMARY KEY (Venue_ID, Phone_Number),
+    FOREIGN KEY (Venue_ID) REFERENCES Venue(ID)
+);
+
+-- Event_Image(VID, EID, Image)
+-- FK VID refers to Venue(ID) (NOT NULL)
+-- FK EID refers to Occasion(ID) (NOT NULL)
+CREATE TABLE Event_Image (
+    Venue_ID int unsigned NOT NULL,
+    Event_ID int unsigned NOT NULL,
+    -- Jose:    I think this should store a link to an image
+    --          You CAN store images as raw data, but a link might be easier
+    E_Image varchar(128),
+    PRIMARY KEY (Venue_ID, Event_ID, E_Image),
+    FOREIGN KEY (Venue_ID) REFERENCES Venue(ID),
+    FOREIGN KEY (Event_ID) REFERENCES Occasion(ID)
+);
+
+-- Event_Price(VID, EID, Price)
+-- FK VID refers to Venue(ID) (NOT NULL)
+-- FK EID refers to Occasion(ID) (NOT NULL)
+CREATE TABLE Event_Price (
+    Venue_ID int unsigned NOT NULL,
+    Event_ID int unsigned NOT NULL,
+    Price float,
+    PRIMARY KEY (Venue_ID, Event_ID, Price),
+    FOREIGN KEY (Venue_ID) REFERENCES Venue(ID),
+    FOREIGN KEY (Event_ID) REFERENCES Occasion(ID)
+);
+
+-- Artist_Genre (AName, Genre)
+-- FK AName refers to Artist(AName)
+CREATE TABLE Artist_Genre (
+    Artist_Name varchar(50) NOT NULL,
+    Genre varchar(25),
+    PRIMARY KEY (Artist_Name, Genre),
+    FOREIGN KEY (Artist_Name) REFERENCES Artist(AName)
+);
+
+-- Customer_PhoneNumber(CID, Phone_Number)
+-- FK CID refers to Customer(ID)
+CREATE TABLE Customer_PhoneNumber (
+    Customer_ID int unsigned NOT NULL
+    Phone_Number varchar(25),
+    PRIMARY KEY (Customer_ID, Phone_Number),
+    FOREIGN KEY (Customer_ID) REFERENCES Customer(ID)
+);
+
+-- Enables foreign keys again
+
+SET foreign_key_checks = 1;
