7bb8adfbffccc4539287e2c55297a89e7d1df7b3
kumar7-hub <snehalkumar18@gmail.com>
2024-04-22 02:48:51 -0700

    home page

--- public/index.php
+++ public/index.php
@@ -1,12 +1,47 @@
+<?php
+    require_once("config.php");
+
+    // Only allow logged-in users to see this page.
+
+    if (!isset($_SESSION["logged_in"])) {
+        header("Location: login.php");
+    }
+
+
+
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Landing Page</title>
+    <title>Home</title>
+    <link rel="stylesheet" href="./style.css">
 </head>
-<body>
-    <span>This is just a simple landing page for the application</span>
-    <p>if we do end up using html/php</p>
-</body>
+    <body>
+        <div class="container">
+
+            <div class="header">
+                <h3 class="glass" id="w-message">Welcome, <?= $_SESSION["user"] ?><h3>
+            </div>
+
+            <ul id="navlist">
+                <li><a href="index.php" style="font-size:1.5vw;">concertRadar</a></li>
+                <li style="float:right;">
+                    <a href="logout.php"><button id="sign-out-btn">Sign Out</button></a>
+                </li>
+                <li style="float:right;"><a href=# style="padding-top:24px;">PopularEvents</a></li>
+                <li style="float:right;"><a href=# style="padding-top:24px;">PopularArtists</a></li>
+            </ul>
+
+
+
+
+
+        </div>
+    </body>
 </html>
